fc7179e8b3f741de2cbd682badc4f2fa64911a2a
unknown <nhile8693@gmail.com>
2019-08-13 17:21:08 +0700

    0813

--- js/practise.js
+++ js/practise.js
@@ -3,39 +3,34 @@
 var num = [1,2,7, "ae"];
 var piece = num.slice(0,4);
 
-var pop_text = document.getElementsByClassName("pop_text").textContent;
 
-//document.getElementById("demo_show").innerText = piece + ", " + pop_text;
-
-//Object
 var customer = {
     name: "salah",
     dob: {
-        month: "06",
         date: "08",
+        month: "06",
         year: "1993"
     },
     job: "Doctor"
 };
 
 var txt ="";
-var x;
-var count=0;
+var len = Object.keys(customer.dob).length;
 
-for(x in customer.dob){
-    txt += customer.dob[x];
+// Object.keys(customer.dob).forEach(function(prop){
 
-    for(count; Object.keys(customer.dob).length -1; count++){
-        txt += "/";
-    }
+//     txt += customer.dob[prop];
+//     console.log(prop);
+
+//     for(let i=0; i<len-1; i++){
+//         txt += "/";
+//     }
+// });
+
+for(let i=0; i<len; i++){
+    txt += Object.keys(customer.dob) + "/";
+    
 }
-console.log();
 
-//document.getElementById("demo_show").innerText = txt;
 
-//Object declare type 2
-var staff = new Object();
-staff.name = "Lily";
-staff.dob = "Oct 15th 1986";
-staff.position = "developer";
-document.getElementById("demo_show").innerText = staff.dob;
+document.getElementById("demo_show").innerText = txt;
